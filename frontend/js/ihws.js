7c5d1e1f512a5d4a3837c723b2d76d8c9e64b159
Niklas <xcb3djok@gmail.com>
2020-12-03 07:02:09 +0000

    home, signup pages and redirection

--- frontend/js/ihws.js
+++ frontend/js/ihws.js
@@ -1,2 +1,15 @@
 
 document.getElementById('home').setAttribute('href', 'https://localhost:5000/')
+
+const serialize_form = form => JSON.stringify(
+	Array.from(new FormData(form).entries()).reduce(
+		(m, [key, value]) => Object.assign(m, {[key]: value}), {})
+);
+
+$('signup').on('submit', function(event) {
+	event.preventDefault();
+	const json = serialize_form(this);
+	console.log(json);
+});
+
+
