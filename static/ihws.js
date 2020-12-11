34ce1564a68046c945713b416c9fab1ba4f77b2e
Niklas <xcb3djok@gmail.com>
2020-12-11 08:22:24 +0000

    Script side changes and html fixes

--- static/ihws.js
+++ static/ihws.js
@@ -6,12 +6,13 @@ function signin() {
 		"email": document.getElementById("email").value,
 		"password": document.getElementById("password").value
 	});
-	
+
 	$.ajax(url='http://localhost:5000/token', settings={
 		method: 'POST',
 		data: data
-	}).always(function() {
-		alert(data);
+	}).always(function(json) {
+		alert(json);
+		token = json[0];
 	});
 }
 
@@ -21,6 +22,8 @@ function signup() {
 		"password": document.getElementById("password").value,
 		"email": document.getElementById("email").value
 	});
+
+	alert(data);
 
 	$.ajax(url='http://localhost:5000/users', settings={
 		method: 'POST',
@@ -32,8 +35,10 @@ function upload() {
 	var data = JSON.stringify({
 		"name": document.getElementById("name").value,
 		"description": document.getElementById("description").value,
-		"filename": document.getElementById("file").value
+		"filename": document.getElementById('file').value.split(/(\\|\/)/g).pop()
 	});
+
+	alert(data);
 
 	$.ajax(url='http://localhost:5000/images', settings={
 		method: 'POST',
